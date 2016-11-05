6ce041c54250a7360e76ca014fa433840a382829
dallin-r-parker <dallin.r.parker@gmail.com>
2016-11-05 00:59:43 -0600

    added 75% of footer

--- public/dist/all.js
+++ public/dist/all.js
@@ -25,27 +25,6 @@ angular.module('dw-store').controller('mainCtrl', ["$scope", "mainService", func
         });
     };
     $scope.getProducts();
-
-    $scope.getClassic = function () {
-        mainService.getClassic().then(function (response) {
-            $scope.classics = response.data;
-        });
-    };
-    $scope.getClassic();
-
-    $scope.getBlkClassic = function () {
-        mainService.getBlkClassic().then(function (response) {
-            $scope.blkclassics = response.data;
-        });
-    };
-    $scope.getBlkClassic();
-
-    $scope.getDapClassic = function () {
-        mainService.getDapClassic().then(function (response) {
-            $scope.dapclassics = response.data;
-        });
-    };
-    $scope.getDapClassic();
 }]);
 'use strict';
 
@@ -94,14 +73,33 @@ angular.module('dw-store').directive('blkClassicHeroDir', function () {
 //restrict with A,E, or AE
 'use strict';
 
-angular.module('dw-store').controller('blkClassicWatchCtrl', ["$scope", function ($scope) {}]);
+angular.module('dw-store').directive('bottomDescriptionDir', function () {
+
+    return {
+        restrict: 'E',
+        templateUrl: 'app/directives/bottomDescription/bottom-description-tmpl.html'
+    };
+});
+//restrict with A,E, or AE
+'use strict';
+
+angular.module('dw-store').controller('blkClassicWatchCtrl', ["$scope", "mainService", function ($scope, mainService) {
+
+    $scope.getBlkClassic = function () {
+        mainService.getBlkClassic().then(function (response) {
+            $scope.blkclassics = response.data;
+        });
+    };
+    $scope.getBlkClassic();
+}]);
 'use strict';
 
 angular.module('dw-store').directive('blkClassicWatchDir', function () {
 
     return {
         restrict: 'E',
-        templateUrl: 'app/directives/blkClassicWatch/blk-classic-watch-tmpl.html'
+        templateUrl: 'app/directives/blkClassicWatch/blk-classic-watch-tmpl.html',
+        controller: 'blkClassicWatchCtrl'
     };
 });
 //restrict with A,E, or AE
@@ -118,30 +116,26 @@ angular.module('dw-store').directive('classicHeroDir', function () {
     };
 });
 //restrict with A,E, or AE
-"use strict";
+'use strict';
+
+angular.module('dw-store').controller('classicWatchCtrl', ["$scope", "mainService", function ($scope, mainService) {
+    $scope.getClassic = function () {
+        mainService.getClassic().then(function (response) {
+            $scope.classics = response.data;
+        });
+    };
+    $scope.getClassic();
+}]);
 'use strict';
 
 angular.module('dw-store').directive('classicWatchDir', function () {
     return {
         restrict: 'E',
         templateUrl: 'app/directives/classicWatch/classic-watch-tmpl.html',
+        controller: 'classicWatchCtrl',
         scope: {
             classics: '='
         }
-    };
-});
-//restrict with A,E, or AE
-'use strict';
-
-angular.module('dw-store').controller('dapWatchCtrl', ["$scope", function ($scope) {}]);
-'use strict';
-
-angular.module('dw-store').directive('dapWatchDir', function () {
-
-    return {
-        restrict: 'E',
-        templateUrl: 'app/directives/dapWatch/dap-watch-tmpl.html',
-        controller: 'dapWatchCtrl'
     };
 });
 //restrict with A,E, or AE
@@ -156,6 +150,38 @@ angular.module('dw-store').directive('dapHeroDir', function () {
         restrict: 'E',
         templateUrl: 'app/directives/dapHero/dap-hero-tmpl.html',
         controller: 'dapHeroCtrl'
+    };
+});
+//restrict with A,E, or AE
+'use strict';
+
+angular.module('dw-store').controller('dapWatchCtrl', ["$scope", "mainService", function ($scope, mainService) {
+
+    $scope.getDapClassic = function () {
+        mainService.getDapClassic().then(function (response) {
+            $scope.dapclassics = response.data;
+        });
+    };
+    $scope.getDapClassic();
+}]);
+'use strict';
+
+angular.module('dw-store').directive('dapWatchDir', function () {
+
+    return {
+        restrict: 'E',
+        templateUrl: 'app/directives/dapWatch/dap-watch-tmpl.html',
+        controller: 'dapWatchCtrl'
+    };
+});
+//restrict with A,E, or AE
+'use strict';
+
+angular.module('dw-store').directive('footerDir', function () {
+
+    return {
+        restrict: 'E',
+        templateUrl: 'app/directives/footer/footer-tmpl.html'
     };
 });
 //restrict with A,E, or AE
